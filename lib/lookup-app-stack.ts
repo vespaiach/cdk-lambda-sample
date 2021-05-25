fdc160790b21d00ec9f2de4836ca3b4e91a9bb23
Toan Nguyen <nta.toan@gmail.com>
2021-05-25 09:44:02 -0400

    fix: stack info

--- lib/lookup-app-stack.ts
+++ lib/lookup-app-stack.ts
@@ -2,14 +2,17 @@ import * as cdk from '@aws-cdk/core';
 import * as dynamodb from '@aws-cdk/aws-dynamodb';
 import * as lambda from '@aws-cdk/aws-lambda';
 import * as gateway from '@aws-cdk/aws-apigateway';
+
+const stageName = process.env.STAGE_NAME || 'dev';
+const allowOrigins = process.env.CORS_ALLOW_ORIGINS || 'http://localhost:8080';
+
 export class LookupAppStack extends cdk.Stack {
     constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
         super(scope, id, props);
 
         const table = new dynamodb.Table(this, 'StorageKeyValue', {
             billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
-            sortKey: { name: 'noteId', type: dynamodb.AttributeType.STRING },
-            partitionKey: { name: 'userId', type: dynamodb.AttributeType.STRING },
+            partitionKey: { name: 'uuid', type: dynamodb.AttributeType.STRING },
         });
 
         const getData = new lambda.Function(this, 'getData', {
@@ -35,10 +38,22 @@ export class LookupAppStack extends cdk.Stack {
 
         const apigw = new gateway.RestApi(this, 'lookup-app-api', {
             restApiName: 'Lookup App Service',
+            deployOptions: {
+                stageName,
+            },
+            defaultCorsPreflightOptions: {
+                allowHeaders: ['Content-Type', 'X-Amz-Date', 'Authorization', 'X-Api-Key'],
+                allowMethods: ['OPTIONS', 'GET', 'POST', 'PUT', 'DELETE'],
+                allowCredentials: true,
+                allowOrigins: allowOrigins.split(','),
+            },
         });
 
-        const res = apigw.root.addResource('lookup');
-        res.addMethod('GET', new gateway.LambdaIntegration(getData));
-        res.addMethod('POST', new gateway.LambdaIntegration(putData));
+        const lookupRes = apigw.root.addResource('lookup');
+
+        lookupRes.addMethod('POST', new gateway.LambdaIntegration(putData));
+        lookupRes.addResource('{uuid}').addMethod('GET', new gateway.LambdaIntegration(getData));
+
+        new cdk.CfnOutput(this, 'apiUrl', { value: apigw.url });
     }
 }
