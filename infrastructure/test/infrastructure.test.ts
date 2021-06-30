bf574aa00666d74f3eb22fd91022c0d3a8c5140f
Toan Nguyen <nta.toan@gmail.com>
2021-06-30 19:53:02 -0400

    feat: add yarn workspace; init projects

--- infrastructure/test/infrastructure.test.ts
+++ infrastructure/test/infrastructure.test.ts
@@ -1,11 +1,11 @@
 import { expect as expectCDK, matchTemplate, MatchStyle } from '@aws-cdk/assert';
 import * as cdk from '@aws-cdk/core';
-import * as LookupApp from '../lib/lookup-app-stack';
+import * as Infrastructure from '../lib/infrastructure-stack';
 
 test('Empty Stack', () => {
     const app = new cdk.App();
     // WHEN
-    const stack = new LookupApp.LookupAppStack(app, 'MyTestStack');
+    const stack = new Infrastructure.InfrastructureStack(app, 'MyTestStack');
     // THEN
     expectCDK(stack).to(matchTemplate({
       "Resources": {}
