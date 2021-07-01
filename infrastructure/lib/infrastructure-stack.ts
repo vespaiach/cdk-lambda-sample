caefa5e23679382c0337d5882dc87c63970f6600
Toan Nguyen <nta.toan@gmail.com>
2021-06-30 23:26:20 -0400

    feat: config app build & deployment

--- infrastructure/lib/infrastructure-stack.ts
+++ infrastructure/lib/infrastructure-stack.ts
@@ -1,9 +1,20 @@
 import * as cdk from '@aws-cdk/core';
+import * as s3Deploy from '@aws-cdk/aws-s3-deployment';
+import * as s3 from '@aws-cdk/aws-s3';
 
 export class InfrastructureStack extends cdk.Stack {
-  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
+    constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+        super(scope, id, props);
 
-    // The code that defines your stack goes here
-  }
+        const webBucket = new s3.Bucket(this, 'WebBucket', {
+            encryption: s3.BucketEncryption.S3_MANAGED,
+        });
+
+        new s3Deploy.BucketDeployment(this, 'DeployWebsite', {
+            prune: true,
+            sources: [s3Deploy.Source.asset('./build')],
+            destinationBucket: webBucket,
+            destinationKeyPrefix: 'web/static', // optional prefix in destination bucket
+        });
+    }
 }
